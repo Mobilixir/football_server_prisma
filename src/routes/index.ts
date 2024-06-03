32b166decdee6cc26fb2e4e5de71712a40804693
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-06-03 17:05:47 +0530

    - Added router, controller for team - Added CRUD operations for team

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
-// src/routes/SetupRoutes.ts
-
 import { Application } from 'express';
+
 import authRoute from './authRoute';
+import teamRoute from './teamRoute';
 
 export default function setupRoutes(app: Application) {
   app.use(authRoute);
+  app.use(teamRoute);
 }
