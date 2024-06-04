2449a78ff00a19b1caade49f519884053cab3a58
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-06-04 10:05:34 +0530

    - Add helper method to exclude some keys from response

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,8 +1,8 @@
 import Jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 import crypto from 'crypto';
+import { Prisma } from '@prisma/client';
 
-// import User from '../model/User';
 import prismaClient from './database';
 import { jwt_token, validity, encryption_algorithm, encryption_key, iv_length } from '../utils/config';
 import eUserType from '../enum/userType';
@@ -98,5 +98,21 @@ const getUserFromToken = async (req: Request, res: Response) => {
   return decrypt(id);
 };
 
-const helper = { isEmpty, createJwtAuthToken, checkUserRole, handleError, getUserFromToken };
+type A<T extends string> = T extends `${infer U}ScalarFieldEnum` ? U : never;
+type Entity = A<keyof typeof Prisma>;
+type Keys<T extends Entity> = Extract<keyof (typeof Prisma)[keyof Pick<typeof Prisma, `${T}ScalarFieldEnum`>], string>;
+
+export function prismaExclude<T extends Entity, K extends Keys<T>>(type: T, omit: K[]) {
+  type Key = Exclude<Keys<T>, K>;
+  type TMap = Record<Key, true>;
+  const result: TMap = {} as TMap;
+  for (const key in Prisma[`${type}ScalarFieldEnum`]) {
+    if (!omit.includes(key as K)) {
+      result[key as Key] = true;
+    }
+  }
+  return result;
+}
+
+const helper = { isEmpty, createJwtAuthToken, checkUserRole, handleError, getUserFromToken, prismaExclude };
 export default helper;
