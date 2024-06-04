b1291195df2a1929c2764f0ae140d17e4b5dc106
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-06-04 21:09:57 +0530

    - WIP on adding routes

---
+++ src/routes/matchRoute.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import MatchController from '../controller/MatchController';
+
+const AllRouter = Router();
+const match = new MatchController();
+
+AllRouter.route('/api/v1/getuserrolelist').get(user.getUserRoleList);
+AllRouter.route('/api/v1/updateuserrole').put(user.updateUserRole);
+AllRouter.route('/api/v1/updateuserteam').put(user.updateUserTeam);
+AllRouter.route('/api/v1/addusertoteam').post(user.addUsersToTeam);
+AllRouter.route('/api/v1/getplayerbyteam/:teamId').get(user.getPlayersByTeamId);
+
+export default AllRouter;
