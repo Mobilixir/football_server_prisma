605c67b260896343d4378fe443bdaccf458f02fe
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-06-17 19:11:28 +0530

    - Match route changes

--- src/routes/matchRoute.ts
+++ src/routes/matchRoute.ts
@@ -4,9 +4,9 @@ import MatchController from '../controller/MatchController';
 const AllRouter = Router();
 const match = new MatchController();
 
-AllRouter.route('/api/v1/creatematch').get(match.createMatch);
+AllRouter.route('/api/v1/creatematch').post(match.createMatch);
 AllRouter.route('/api/v1/updatematch').put(match.updateMatch);
-AllRouter.route('/api/v1/deletematch').put(match.deleteMatch);
+AllRouter.route('/api/v1/deletematch/:matchId').delete(match.deleteMatch);
 AllRouter.route('/api/v1/getallmatches').get(match.getAllMatches);
 AllRouter.route('/api/v1/getmatch/:matchId').get(match.getMatchById);
 AllRouter.route('/api/v1/getmatch/team/:teamId').get(match.getMatchesByTeamId);
